b9c67d05b8f4a9e4811dd998e96fc72f2fc8875b
Tim <timothy.bott87@gmail.com>
2020-08-12 10:54:10 +0200

    Create AppServiceProvider.php

---
+++ AppServiceProvider.php
@@ -0,0 +1,30 @@
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Laravel db fix
+        Schema::defaultStringLength(191);
+        \URL::forceScheme('https');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (env('APP_INSTALLED') && env('APP_DEBUG')) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
+
+        if (env('APP_ENV') !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+    }
+}
